c13af6df0ed1e8060c66aa98cbccf0076ca2df03
imc <i_cenov@botevgrad.com>
2025-08-20 18:13:59 +0300

    Added comments to ota_start.bat

--- ota_start.bat
+++ ota_start.bat
@@ -1,3 +1,11 @@
+rem http_server starter for runing behind Apache SSL reverse proxy virtual host.
+
+@echo off
+rem Edit next line according your actual environment.
 set PYTHONHOME=c:\Python\Python313
+
 @call .venv\Scripts\activate.bat
+
+rem Staring server with no certificates and with token authentication.
+rem Run python http_server.py to see all options.
 python http_server.py --port 8071 --no-certs
