1a516b7da17def165a2652b2198297a308da7d39
imc <i_cenov@botevgrad.com>
2026-08-05 20:53:32 +0300

    RT #331: Updated DatabaseInterface,

--- src/ota_http_server/database/database_interface.py
+++ src/ota_http_server/database/database_interface.py
@@ -2,6 +2,8 @@
 
 from typing import Protocol
 
+from ota_http_server.core.data_models import User
+
 class DatabaseInterface(Protocol):
 
     def init_db(self) -> None:
@@ -13,3 +15,26 @@ class DatabaseInterface(Protocol):
     def rollback(self) -> None:
         ...
 
+    def add_user(self, user: User) -> User:
+        ...
+
+    def enable_user_by_id(self, user_id: int) -> None:
+        ...
+
+    def enable_user_by_username(self, username: str) -> None:
+        ...
+
+    def disable_user_by_id(self, user_id: int) -> None:
+        ...
+
+    def disable_user_by_username(self, username: str) -> None:
+        ...
+
+    def user_get_by_id(self, user_id: int) -> User | None:
+        ...
+
+    def user_get_by_username(self, username: str) -> User | None:
+        ...
+
+    def user_get_list(self) -> list[User]:
+        ...
